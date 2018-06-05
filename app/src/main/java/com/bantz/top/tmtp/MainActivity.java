2ced193fa32860fa445ba37a73574c5a03d4a3d7
Cosmin <marnicos2006@yahoo.com>
2018-06-05 14:13:37 +0100

    Working Audio Permissions

--- app/src/main/java/com/bantz/top/tmtp/MainActivity.java
+++ app/src/main/java/com/bantz/top/tmtp/MainActivity.java
@@ -3,14 +3,20 @@ package com.bantz.top.tmtp;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Message;
 import android.support.constraint.ConstraintLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
+import android.webkit.PermissionRequest;
 import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -18,10 +24,15 @@ import com.onesignal.OneSignal;
 
 import im.delight.android.webview.AdvancedWebView;
 
+import static android.Manifest.permission.RECORD_AUDIO;
+
 public class MainActivity extends Activity implements AdvancedWebView.Listener{
+
+    private static final int MY_PERMISSIONS_REQUEST_RECORD_AUDIO = 101;
 
     private AdvancedWebView mWebView;
     private ConstraintLayout layout;
+    private PermissionRequest myRequest;
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
@@ -60,7 +71,26 @@ public class MainActivity extends Activity implements AdvancedWebView.Listener{
         mWebView.setWebChromeClient(new WebChromeClient() {
 
             @Override
+            public void onPermissionRequest(final PermissionRequest request) {
+                // TODO: inspect and verify the request
+                myRequest = request;
+
+                for (String permission : request.getResources()) {
+                    switch (permission) {
+                        case "android.webkit.resource.AUDIO_CAPTURE": {
+                            askForPermission(request.getOrigin().toString(), RECORD_AUDIO, MY_PERMISSIONS_REQUEST_RECORD_AUDIO);
+                            break;
+                        }
+                    }
+                }
+                //request.grant(request.getResources());
+            }
+
+
+
+            @Override
             public boolean onCreateWindow(WebView view, boolean isDialog, boolean isUserGesture, Message resultMsg) {
+
                 AdvancedWebView newWebView = new AdvancedWebView(MainActivity.this);
                 // myParentLayout.addView(newWebView, LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
                 WebView.WebViewTransport transport = (WebView.WebViewTransport) resultMsg.obj;
@@ -75,6 +105,31 @@ public class MainActivity extends Activity implements AdvancedWebView.Listener{
                 .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
                 .unsubscribeWhenNotificationsAreDisabled(true)
                 .init();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_RECORD_AUDIO: {
+                Log.d("WebView", "PERMISSION FOR AUDIO");
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    myRequest.grant(myRequest.getResources());
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+            }
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 
     @SuppressLint("NewApi")
@@ -144,5 +199,33 @@ public class MainActivity extends Activity implements AdvancedWebView.Listener{
         layout.removeView(mWebView);
         mWebView.removeAllViews();
         mWebView.destroy();
+    }
+
+    public void askForPermission(String origin, String permission, int requestCode) {
+        Log.d("WebView", "inside askForPermission for" + origin + "with" + permission);
+
+        if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                permission)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    permission)) {
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{permission},
+                        requestCode);
+            }
+        } else {
+            myRequest.grant(myRequest.getResources());
+        }
     }
 }
